1ef371d01811a256bf24260ff36ec7d8ebb0c528
Patrik Poklop <151776170+imang212@users.noreply.github.com>
2025-05-01 08:48:17 +0200

    Add files via upload

--- DLH_exoplanets.py
+++ DLH_exoplanets.py
@@ -6,21 +6,35 @@ import pandas as pd
 # connect to DuckDB
 con = duckdb.connect()
 
-# Load the CSV file into a DucDB table
 db = duckdb.read_csv(r"C:\Users\imang\OneDrive\Plocha\ODM\adityamishraml\nasaexoplanets\versions\2\cleaned_5250.csv")
+duckdb.sql("SELECT * FROM 'db'").show()
 
-
+db2 = duckdb.read_csv("PS_2025.04.28_06.13.44.csv")
 duckdb.sql("SELECT * FROM 'db'").show()
 
 con.execute("""
         CREATE TABLE exoplanets AS
-        SELECT *
-        FROM read_csv_auto('adityamishraml\nasaexoplanets\versions\2\cleaned_5250.csv');
+        SELECT e.*, n.pl_pubdate, n.releasedate
+        FROM read_csv_auto('adityamishraml/nasaexoplanets/versions/2/cleaned_5250.csv') e
+        LEFT JOIN read_csv_auto("PS_2025.04.28_06.13.44.csv") n ON LOWER(e.name) = LOWER(n.pl_name)
     """)
 
-print(query = con.execute("""
-        SELECT name, discovery_year, planet_type
-        FROM db
+
+print(con.execute("""
+        SELECT name, discovery_year
+        FROM exoplanets
         WHERE discovery_year > 2010
         ORDER BY discovery_year ASC
     """).fetchdf())
+
+
+con.execute("""
+    CREATE TABLE dim_planet_type AS
+    SELECT DISTINCT planet_type
+    FROM exoplanets;
+""")
+
+# lakehouse storage
+con.execute("""
+    COPY exoplanets TO 'exoplanets.parquet' (FORMAT 'parquet');
+""")
